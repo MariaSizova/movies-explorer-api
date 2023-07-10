f07f956b32435f671a7ebbb133b9c335d8aa2cb8
Maria Sizova <sizova.maria.olegovna@yandex.ru>
2023-07-10 11:55:08 +0300

    Поправлены ошибки в корсе

--- middlewares/corsHandler.js
+++ middlewares/corsHandler.js
@@ -1,8 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'https:api.diplom.marisizova.nomoredomains.rocks/',
-  'http:api.diplom.marisizova.nomoredomains.rocks/',
-  'https://api.diploma-project.nomoredomains.rocks/',
+  'https://api.diplom.marisizova.nomoredomains.rocks/',
+  'http://api.diplom.marisizova.nomoredomains.rocks/',
   'https://localhost:3000',
   'http://localhost:3000',
 ];
